bc59319e7747d93829d6d195983079993ce61082
mervanhasanaj <63062971+mervanhasanaj@users.noreply.github.com>
2021-11-04 10:58:34 +0100

    Livello 4

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -3,6 +3,7 @@ from prima_app. views import homepage
 from prima_app.views import welocome
 from prima_app.views import lista
 from prima_app.views import chi_siamo
+from prima_app.views import variabili
 
 app_name="prima_app"
 urlpatterns=[
@@ -10,4 +11,5 @@ path('', homepage, name='homepage'),
 path('welcome', welocome, name='welcome'),
 path('lista', lista, name='lista'),
 path('chi_siamo', chi_siamo, name='chi_siamo'),
+path('variabili', variabili, name='variabili'),
 ]
