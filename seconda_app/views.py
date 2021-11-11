d240f6f969c200696e58a27d5146e0a6b994dbe2
mervanhasanaj <63062971+mervanhasanaj@users.noreply.github.com>
2021-11-11 10:38:36 +0100

    Livello 8

--- seconda_app/views.py
+++ seconda_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 def es_if(request):
@@ -16,3 +17,10 @@ def if_else_elif(request):
         'var3': 150.50,
     }
     return render(request, "if_else_elif.html", context)
+
+def es_for(request):
+    context={
+        'list1': [1, datetime.date(2019,7,16), 'Do not give up!'],
+        'list2': [1, datetime.date(2019,7,16), 'Do not give up!'],
+    }
+    return render(request, "es_for.html", context)
